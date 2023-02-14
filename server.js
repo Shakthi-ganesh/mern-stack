18789019712cc45d9d081adc0a89b1bd1f708bae
Shakti <shakthiganeshpedapudi@gmail.com>
2023-02-14 20:02:53 +0530

    adding

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res){
 	//res.send("Iam shakthi");
-	res.sendFile(__dirname+"/index.html")
+	res.sendFile(__dirname+"/resume.html")
 });
 app.get("/resume.html", function(req, res){
 	//res.send("Iam shakthi");
