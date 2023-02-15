b626789cf50c3a5d55a02e7899392bdaf831f99a
Shakti <shakthiganeshpedapudi@gmail.com>
2023-02-15 21:56:28 +0530

    Adding Data base

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+//make sure env file that becomes 
+const mongoose=require("mongoose");
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
@@ -6,12 +9,24 @@ app.get("/", function(req, res){
 	//res.send("Iam shakthi");
 	res.sendFile(__dirname+"/index.html")
 });
-app.get("/resume.html", function(req, res){
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
+	if(err)
+	{
+		console.error(err);
+		
+	}
+	else
+	{
+		console.log("DB CONNECTED");
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});
+		
+	}
+});
+app.get("/resume", function(req, res){
 	//res.send("Iam shakthi");
 	res.sendFile(__dirname+"/resume.html")
-});
-
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
 });
