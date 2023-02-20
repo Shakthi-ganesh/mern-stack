7cb56cdd4e0ff8896f6d2820f2eab15b79c3fa3c
Shakti <shakthiganeshpedapudi@gmail.com>
2023-02-20 19:56:48 +0530

    added js to weather

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,33 +1,37 @@
-console.log("hello from js");
-document.getElementById("loader").style.display="none";
+console.log("Hello from JS");
+
+//document.getElementById("loader").style.display = "none";
+
+document.getElementById("loader").style.display = "block";
 
 fetch("/api/todos")
-   .then(function(response)
-    {
-    return response.json();
+    .then(function(response){
+        return response.json();
     })
-    .then(function(date)
-    {
+
+    .then(function(data){
         console.log(data);
-        document.getElementById("loader").style.display="none";
+
+        document.getElementById("loader").style.display = "none";
+
 
     });
-    var light=true;
-    function setTheme()
-    {
-        if(light)
-        {
-           document.documentElement.setAttribute("data-bs-theme","dark");
-           
-        }
 
+
+    var light = true;
+
+
+    function setTheme(){
+        if(light){
+            document.documentElement.setAttribute("data-bs-theme","dark");
+            document.getElementById("themeButton").innerHTML = '<i class="fas fa-sun fa-lg fa-fw"></i></button> ';
+        }
         else{
             document.documentElement.setAttribute("data-bs-theme","light");
-
+            document.getElementById("themeButton").innerHTML = '<i class="fas fa-moon fa-lg fa-fw"></i></button> ';
         }
-        light=!light;
+        light = ! light;
     }
-  
 //to hide  leader
 
 //to show loader
